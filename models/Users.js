64fad252ea1e21ad0da97f7752c74cd26df51395
fouadmen <mannoufouad@outlook.com>
2019-10-30 21:47:24 +0900

    add connection string

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,9 @@
 'use strict';
 const mongoose = require('mongoose');
 //TODO: adapt mongodb to use case
-mongoose.connect('mongodb://localhost:27017/SeseragiDB',{useNewUrlParser: true, useUnifiedTopology:true})
+const DEBUG = false;
+const connectionString = DEBUG ? 'mongodb://localhost:27017/SeseragiDB' : 'mongodb://heroku_xbjkx44m:B9nQT9iTAb@ds241408.mlab.com:41408/heroku_xbjkx44m';
+mongoose.connect(connectionString,{useNewUrlParser: true, useUnifiedTopology:true})
     .then(()=>console.log('Connected to MongoDB'))
     .catch(()=>console.log('Cannot connect to MongoDB'));
 
