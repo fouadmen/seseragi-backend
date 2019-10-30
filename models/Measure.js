d16fb77f91bac252ecfeb555baf5544c727febf2
fouadmen <mannoufouad@outlook.com>
2019-10-30 21:51:21 +0900

    log connection string

--- models/Measure.js
+++ models/Measure.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 //TODO: adapt mongodb to use case
 const DEBUG = false;
 const connectionString = DEBUG ? 'mongodb://localhost:27017/SeseragiDB' : 'mongodb://heroku_xbjkx44m:B9nQT9iTAb@ds241408.mlab.com:41408/heroku_xbjkx44m';
+console.log(connectionString);
 mongoose.connect(connectionString,{useNewUrlParser: true, useUnifiedTopology:true})
     .then(()=>console.log('Connected to MongoDB'))
     .catch(()=>console.log('Cannot connect to MongoDB'));
