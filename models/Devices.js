acf9d75f0ff328cb082986fac7e5ba19ebeb1425
fouadmen <mannoufouad@outlook.com>
2019-11-03 02:02:32 +0900

    change db connectionString

--- models/Devices.js
+++ models/Devices.js
@@ -1,8 +1,8 @@
 'use strict';
 const mongoose = require('mongoose');
 //TODO: adapt mongodb to use case
-const DEBUG = true;
-const connectionString = DEBUG ? 'mongodb://localhost:27017/SeseragiDB' : 'mongodb://seseragiDb:B9nQT9iTAb@ds241408.mlab.com:41408/heroku_xbjkx44m';
+const DEBUG = false;
+const connectionString = DEBUG ? 'mongodb://localhost:27017/SeseragiDB' : 'mongodb://seseragi:L4u6yXNdzHxsRXww@ds241258.mlab.com:41258/heroku_bphq3jd9';
 mongoose.connect(connectionString,{useNewUrlParser: true, useUnifiedTopology:true})
     .then(()=>console.log('Connected to MongoDB'))
     .catch(()=>console.log('Cannot connect to MongoDB'));
