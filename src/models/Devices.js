cba80093304560f1396562875533d6e1de784bf5
fouadmen <mannoufouad@outlook.com>
2020-02-13 13:40:12 +0900

    remove unique embedded ref

--- src/models/Devices.js
+++ src/models/Devices.js
@@ -8,7 +8,6 @@ const DeviceSchema = new mongoose.Schema({
     user: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            unique: true,
             ref:'User'
         }
     ],
