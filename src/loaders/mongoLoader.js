c0bf7c2a81473901d4f95781835157f084dfcd68
fouadmen <mannoufouad@outlook.com>
2020-02-13 13:42:19 +0900

    Remove index generation

--- src/loaders/mongoLoader.js
+++ src/loaders/mongoLoader.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 //change to mongo
 const config = require('../config');
 module.exports = async ()=>{
-     await mongoose.connect(config.connectionString,{useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false})
+     await mongoose.connect(config.connectionString,{useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:false, useFindAndModify:false})
         .then(()=>console.log('Connected to MongoDB'))
-        .catch(()=>console.log('Cannot connect to MongoDB'));
+        .catch((err)=>console.log('Cannot connect to MongoDB', err));
 };
